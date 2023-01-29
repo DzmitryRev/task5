c0748bb184c37c07f1afb6cea689ed40b7bc0e11
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-29 14:36:35 +0300

    feat: add supported locales const

---
+++ src/const/locales.ts
@@ -0,0 +1,14 @@
+
+/**
+ * if you want to add new locale to you app - just add it in SUPPORTED_LOCALES
+ * if UsableLocale don't support value from SUPPORTED_LOCALES const -
+ * they should be ignore
+ * import { UsableLocale } from "@faker-js/faker"
+ */
+export const SUPPORTED_LOCALES = ["en", "ru", "de", "cz", "pl"] as const;
+
+export type ElementType<T extends ReadonlyArray<unknown>> = T extends ReadonlyArray<
+  infer ElementType
+>
+  ? ElementType
+  : never;
