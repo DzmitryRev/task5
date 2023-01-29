131ca41f01c5430fceba66e8cebc4b929120921a
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-29 15:57:24 +0300

    feat: add Header components

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Box, FormControlLabel, Typography } from "@mui/material";
+import { MaterialUISwitch } from "../styles/MaterialUIThemeSwitcher";
+
+interface IHeaderProps {
+  changeTheme(): void;
+}
+
+export default function Header({ changeTheme }: IHeaderProps) {
+  return (
+    <Box
+      sx={{
+        maxWidth: "1000px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        px: 5,
+        py: 2,
+        mb: 3,
+      }}
+    >
+      <Typography variant="h6">Random users app</Typography>
+      <FormControlLabel
+        sx={{ m: 0, width: "fit-content" }}
+        control={<MaterialUISwitch onChange={changeTheme} defaultChecked />}
+        label=""
+      />
+    </Box>
+  );
+}
